84e10ecee18405ce16380a630f0727c257e145ab
liuwenlong <liuwenlong2020@qq.com>
2023-08-22 23:44:37 +0800

    strategy

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -1,1 +1,37 @@
 package strategy
+
+import "fmt"
+
+type Strategy interface {
+	fun()
+}
+
+var StrategyMap map[string]Strategy
+
+func init() {
+	StrategyMap = map[string]Strategy{
+		"1": &Strategy1{},
+		"2": &Strategy2{},
+	}
+}
+
+func NewStrategy(strategyType string) (Strategy, error) {
+	if val, ok := StrategyMap[strategyType]; ok {
+		return val, nil
+	}
+	return nil, fmt.Errorf("No supported")
+}
+
+// 策略1
+type Strategy1 struct{}
+
+func (s *Strategy1) fun() {
+	fmt.Println(1)
+}
+
+// 策略2
+type Strategy2 struct{}
+
+func (s *Strategy2) fun() {
+	fmt.Println(2)
+}
--- strategy/strategy_test.go
+++ strategy/strategy_test.go
@@ -1,1 +1,12 @@
 package strategy
+
+import "testing"
+
+func TestNewStrategy(t *testing.T) {
+	s, err := NewStrategy("3")
+	if err != nil {
+		t.Logf("err: %+v", err)
+		return
+	}
+	s.fun()
+}
